caa74a5b6a4526821c0a973f41afc2635074ebff
Rumman Adnan <109212385+rumman-adnan@users.noreply.github.com>
2023-01-05 00:06:53 +0500

    Add files via upload

--- day_3/05_variables.py
+++ day_3/05_variables.py
@@ -14,23 +14,6 @@ x= 5  # Numeric or integer variable
 # print(type(x))
 # print(type(y))
 
-# 1. it 
-
-
-
-
-# 2. Dont ever write variables with spaces instead use _
-# 3. Dont use capital letters
-# 4. always use short words (max 2) e.g samosa
-# 5. Dont start with special characters
-# 6. Don't use numbers in variables at beginning
-# 7. meaning ful names
-# 8. Check global trends (df=dataframe)
-# 9. Dont ever repeat same variable
-# 10. Dont use operators ()
-# 11. Any variable name update to new variable name
-# 12. Dont use "" when assigning names
- 
 
 
 """
@@ -47,6 +30,9 @@ fruit_basket = "Mangoes", "Oranges"
 del fruit_basket
 # print(fruit_basket)
 
+
+# Lecture work
+
 x = 2+3+9-9*12*(2/3)
 fruit_basket = "Mangoes", "Oranges"
 samosa = ('Aalo', 'Piaz', 'Sabz Mirch', 'Dhania', 'Keema', 'Anardana', 'Maida')
@@ -56,4 +42,27 @@ samosa = ('Aalo', 'Piaz', 'Sabz Mirch', 'Dhania', 'Keema', 'Anardana', 'Maida')
 print(type(x))
 print(type(fruit_basket))
 print(type(samosa))
+x = int(x)   # type conversion implicit
+print(type(x))
 
+# Pros: 
+# 1. it reduces human effort for coding
+# 2. Easy to write for long datasets
+# 3. Easy to recall
+
+
+
+
+# 1. Use those keywords which are easy to remember not python keywords
+# 2. Dont ever write variables with spaces instead use _
+# 3. Dont use capital letters
+# 4. always use short words (max 2) e.g samosa
+# 5. Dont start with special characters
+# 6. Don't use numbers in variables at beginning
+# 7. meaning ful names
+# 8. Check global trends (df=dataframe)
+# 9. Dont ever repeat same variable
+# 10. Dont use operators ()
+# 11. Any variable name update to new variable name
+# 12. Dont use "" when assigning names
+ 
